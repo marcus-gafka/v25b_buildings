d2defde81ca1a95baa985aa9466a89ed5b73fc9e
Marcus <msgafka@gmail.com>
2025-10-27 21:09:51 +0100

    checking git link

---
+++ plotter.py
@@ -0,0 +1,30 @@
+import json
+import matplotlib.pyplot as plt
+from shapely.geometry import shape
+
+# --- Load GeoJSON file ---
+with open("V25B_Buildings_Primary_Building_Dataset.geojson") as f:
+    data = json.load(f)
+
+# --- Extract building geometries ---
+fig, ax = plt.subplots()
+for feature in data["features"]:
+    geom = shape(feature["geometry"])
+
+    # Handle polygons and multipolygons
+    if geom.geom_type == "Polygon":
+        x, y = geom.exterior.xy
+        ax.plot(x, y, color="black", linewidth=0.5)
+    elif geom.geom_type == "MultiPolygon":
+        for poly in geom.geoms:
+            x, y = poly.exterior.xy
+            ax.plot(x, y, color="black", linewidth=0.5)
+
+# --- Style plot ---
+ax.set_aspect("equal", adjustable="box")
+ax.grid(True, linestyle="--", linewidth=0.5)
+plt.title("Building Footprints")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+
+plt.show()
